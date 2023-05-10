edef0bbf09fd45b6019b88a03a1d2cf556975a30
Hannah Elder <Hannah.Elder@vu.com>
2023-05-10 14:50:44 -0500

    v1 - classes - too easy!

--- classy_person.py
+++ classy_person.py
@@ -8,8 +8,26 @@ don't forget the use of 'self' and to have the methods:
 '''
 
 class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    
+    def increase_age(self):
+        self.age = self.age + 1
 
+    def say_greeting(self):
+        print(f"Hello world! My name is {self.name}!")
+    
+    def count_to_age(self):
+        counter = 0 
+        for a in range(self.age):
+            counter = counter + 1
+            print(counter)
 
+p = Person("Hannah", 27)
+print(p.age)
+p.say_greeting()
+p.count_to_age()
 
 
 # You won't need to call anything here.
